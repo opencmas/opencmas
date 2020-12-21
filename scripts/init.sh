b821c4ef1550d25e8b5e871e95ad98bb5d920d86
openrobl <stefan.raffl@protonmail.com>
2020-12-21 12:42:53 -0500

    -added initialization script

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,4 +1,18 @@
-#/bin/bash 
-sudo 
+#/bin/bash
+id=$(id -G | egrep -i '^([0])$')
+if [ -z "$id" ]
+then
 
+echo "please execute this script as root"
+
+
+
+else
+   ./base.sh
+    sudo useradd -m cmas -c "openCMAS User" -s /bin/bash 
+    sudo usermod -aG sudo cmas
+    sudo echo "cmas ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+    echo "Please set a Password for the user cmas; command: passwd cmas"
+fi
 exit 0
+
